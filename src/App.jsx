9053d4cf04f5898369a60c72f7ed942b418eb6a5
Priyansh9506 <priyanshpatel9506@gmail.com>
2026-02-17 20:05:05 +0530

    feat: officially remove PreLaunch gate and associated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState, useEffect } from 'react'
+import React, { Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import StaggeredMenu from './components/StaggeredMenu'
 import Home from './pages/Home'
@@ -7,13 +7,9 @@ import Contact from './pages/Contact'
 import Gallery from './pages/Gallery'
 import BoardMembers from './pages/BoardMembers'
 import JoinUs from './pages/JoinUs'
-import PreLaunch from './pages/PreLaunch'
 import { Analytics } from "@vercel/analytics/react"
 
 const CodeBlue = React.lazy(() => import('./pages/CodeBlue'))
-
-
-const TARGET_DATE = "2026-02-17T20:00:00+05:30";
 
 const menuItems = [
   { label: "Home", link: "/" },
@@ -29,29 +25,7 @@ const socialItems = [
   { label: "LinkedIn", link: "https://www.linkedin.com/company/ieee-student-branch-nirma-university" },
 ];
 
-
 const App = () => {
-  const [isLocked, setIsLocked] = useState(new Date() < new Date(TARGET_DATE));
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      if (new Date() >= new Date(TARGET_DATE)) {
-        setIsLocked(false);
-        clearInterval(timer);
-      }
-    }, 1000);
-    return () => clearInterval(timer);
-  }, []);
-
-  if (isLocked) {
-    return (
-      <>
-        <PreLaunch onUnlock={() => setIsLocked(false)} />
-        <Analytics />
-      </>
-    );
-  }
-
   return (
     <div style={{ minHeight: '100vh', backgroundColor: '#000000', position: 'relative' }}>
       <Analytics />
