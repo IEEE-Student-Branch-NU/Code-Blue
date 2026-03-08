fbb13a96d7535da659e051d193573fb54370cf48
nandinirathod2321 <nandinirathod2321@gmail.com>
2026-03-08 12:32:04 +0530

    Exact reproduction of React Bits Electric Border and fix CSS import error

--- src/components/ElectricBorder.jsx
+++ src/components/ElectricBorder.jsx
@@ -3,10 +3,13 @@ import './ElectricBorder.css';
 
 const ElectricBorderCard = ({ children, className = '' }) => {
   return (
-    <div className={`electric-border-wrapper ${className}`}>
-      <div className="electric-border-animated" />
-      <div className="electric-border-inner">
-        {children}
+    <div className={`electric-border-container ${className}`}>
+      <div className="electric-border-glow" />
+      <div className="electric-border-wrapper">
+        <div className="electric-border-animated" />
+        <div className="electric-border-inner">
+          {children}
+        </div>
       </div>
     </div>
   );
